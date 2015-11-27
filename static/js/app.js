a95baff8c5990650f673a5d2b1c8f7befb7e1134
PraveenGajula <siegipraveen@gmail.com>
2015-11-27 14:40:17 +0100

    Delete app.js

--- static/js/app.js
+++
@@ -1,9 +0,0 @@
-
-'use strict'
-var app = angular.module('testApp', ['textAngular']);
-console.log(app);
-
-app.config(['$interpolateProvider', function ($interpolateProvider) {
-    $interpolateProvider.startSymbol('[[');
-    $interpolateProvider.endSymbol(']]');
-}]);
