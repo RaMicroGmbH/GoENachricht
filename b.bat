06979316c2695fcddbc2d876d3915f07f83ce937
PraveenGajula <siegipraveen@gmail.com>
2016-02-19 12:32:21 +0100

    Delete b.bat

--- b.bat
+++
@@ -1,6 +0,0 @@
-go-bindata -pkg="assets" views\. views\base\.
-move /Y bindata.go system\assets\bindata.go
-go-bindata-assetfs -pkg="assetfs" static\css\. static\css\images\. static\img\. static\css\images\igHtmlEditor\. static\fonts\. static\js\. 
-move /Y bindata_assetfs.go system\assetfs\bindata_assetfs.go
-go build 
-
